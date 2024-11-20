68002eb5a6098b17b26d142fff244fd9b3292d15
febmithilesh <mithileshfeb2001@gmail.com>
2024-11-20 11:58:36 +0530

    change the home image path form assets to public

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import Button from "../layouts/Button";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center lg:px-32 px-5 text-white bg-[url('assets/img/Home.png')] bg-no-repeat bg-cover bg-center opacity-90">
+    <div className="min-h-screen flex flex-col justify-center lg:px-32 px-5 text-white bg-[url('/home.png')] bg-no-repeat bg-cover bg-center opacity-90">
       <div className="w-full lg:w-4/5 space-y-5 mt-10 text-center lg:text-left">
         {/* Header */}
         <h1 className="text-3xl lg:text-5xl font-bold leading-tight">
@@ -26,6 +26,7 @@ const Home = () => {
 };
 
 export default Home;
+
 
 
 
